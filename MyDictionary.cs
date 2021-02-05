a1ff4a8692c74b63029a59db2a4332f2225155d4
gunnurraksoy <77543413+gunnurraksoy@users.noreply.github.com>
2021-02-05 13:29:31 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] key;
+        V[] value;
+
+        K[] tempKey;
+        V[] tempValue;
+
+        public MyDictionary()
+        {
+            key = new K[0];
+            value = new V[0];
+        }
+
+        public void Add(K keyItem, V valueItem) 
+        {
+            tempKey = key;
+            
+            key=new K[key.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+            }
+            key[key.Length - 1] = keyItem;
+
+
+
+            tempValue = value;
+
+            value = new V[value.Length + 1];
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                value[i] = tempValue[i];
+            }
+            value[value.Length - 1] = valueItem;
+
+        }
+        public int Count
+        {
+            get { return key.Length; }
+        }
+
+        public void ShowList()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine("Name:" + key[i] + " Age: " + value[i]);
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> NameAge = new MyDictionary<string, int>();
+            NameAge.Add("Günnur", 21);
+            NameAge.Add("Esra", 20);
+            NameAge.Add("Ufuk", 32);
+            NameAge.Add("Mehmet", 25);
+
+
+            Console.WriteLine(NameAge.Count);
+
+            NameAge.ShowList();
+
+            
+        }
+    }
+}
